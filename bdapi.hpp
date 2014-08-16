5eac735838e687e35aa8a03e6efbb1817c459b49
bdapi <sigvatr@gmail.com>
2014-08-16 23:00:27 +1000

    Main Header Commit

---
+++ bdapi.hpp
@@ -0,0 +1,24 @@
+
+/* bdapi - brain damage api version 0.6.0 */
+#ifndef BDAPI_BDAPI_HPP
+#define BDAPI_BDAPI_HPP
+
+/* includes */
+
+// standard
+#include "cstdarg"
+#include "cstdio"
+#include "cstdlib"
+#include "cstring"
+
+// bdapi
+#include "core/build.hpp"
+#include "core/typedefs.hpp"
+#include "core/forward.hpp"
+#include "core/macros_1.hpp"
+#include "core/macros_2.hpp"
+#include "core/macros_3.hpp"
+
+/* end */
+
+#endif
